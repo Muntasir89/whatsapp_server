73e356b57c3412b44ae4f2f411e714ab892e8fae
Muntasir Hossen <montachirhossen76@gmail.com>
2023-08-31 23:54:09 +0600

    whatsApp server

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+var http = require("http");
+// const cors =require("cors");
+const app = express();
+const port = process.env.PORT || 5000;
+var server = http.createServer(app);
+var io = require("socket.io")(server);
+
+// midleware
+app.use(express.json());
+var clients = {};
+// app.use(cors()); npm 
+io.on("connection", (socket) => { 
+
+    console.log("Connected");
+    console.log(socket.id, "has joined");
+    socket.on("signin", (id)=>{
+        console.log(id);
+        clients[id] = socket; 
+        console.log(clients);
+    });
+    socket.on("message", (msg)=>{ 
+        console.log(msg);
+        let targetId = msg.targetId;
+        clients[targetId].emit("message", msg);
+    });
+});
+
+server.listen(port, "0.0.0.0", () => { 
+    console.log("server started");
+});
